ba79cc2d33836d0ffe65d7b92ed56ae702531bb7
Matiaz-hp <huanuirmatiaz@gmail.com>
2025-12-15 21:51:03 -0500

    Create firebase-config.js

---
+++ scripts/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDUxyUkvBOda8HmnQVQ2IdpnClntvMtSI",
+  authDomain: "app-reservas-9e680.firebaseapp.com",
+  projectId: "app-reservas-9e680",
+  storageBucket: "app-reservas-9e680.firebasestorage.app",
+  messagingSenderId: "669976941208",
+  appId: "1:669976941208:web:086a7c3f5d7330250eb6ed",
+  measurementId: "G-1RLMESE1VY"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
